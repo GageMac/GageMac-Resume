442c6bfa3174844f5a6ded8f9a711da250605e3b
Gage Machado <gwm32@humboldt.edu>
2023-05-02 21:02:17 -0700

    Built new component 'Exp' and made changes to Carousel

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ import About from './components/About';
 import Footer from './components/Footer';
 import StickyNav from './components/StickyNav';
 import Contact from './components/Contact';
-import Projects from './components/Projects';
+import Carousel from './components/Carousel';
+import image1 from './assets/GlobalWarming.jpg';
+import image2 from './assets/OSRS-Bot.jpg';
+import image3 from './assets/Stock-Market.jpg';
+import Exp from './components/Exp';
 import './App.css';
 
 function App() {
@@ -16,15 +20,34 @@ function App() {
       <StickyNav />
       <div className="container">
         <div className="column-left">
-          <Experience />
+          <About />
           <Contact />
         </div>
         <div className="column-right">
           <Education />
-          <About />
-          <Projects />
+          <Carousel items={[
+            {
+              id: 1,
+              title: 'Global Warming Data Analysis',
+              image: image1,
+              description: 'Linear Regression Project',
+            },
+            {
+              id: 2,
+              title: 'Project 2',
+              image: image2,
+              description: 'This is project 2',
+            },
+            {
+              id: 3,
+              title: 'Project 3',
+              image: image3,
+              description: 'This is project 3',
+            },
+          ]} />
         </div>
       </div>
+      <Exp />
       <Footer />
     </div>
   );
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,56 +1,71 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState } from 'react';
 import './Carousel.css';
-import myGitHubImage from '../assets/Github.webp';
-import myLinkedinImage from '../assets/Linkedin.png';
 
-function Carousel() {
-  const [currentSlide, setCurrentSlide] = useState(0);
-  const images = [
-    {
-      src: myGitHubImage,
-      alt: 'Project 1',
-      link: 'https://github.com/project1'
-    },
-    {
-      src: myLinkedinImage,
-      alt: 'Project 2',
-      link: 'https://github.com/project2'
-    },
-    {
-      src: 'https://via.placeholder.com/150',
-      alt: 'Project 3',
-      link: 'https://github.com/project3'
-    }
-  ];
+function Carousel({ items }) {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const length = items.length;
 
-  const totalSlides = images.length;
+  const goToPrevSlide = () => {
+    let index = activeIndex;
+    index = index === 0 ? length - 1 : index - 1;
+    setActiveIndex(index);
+  };
 
-  const handleScroll = useCallback(
-    (e) => {
-      e.preventDefault();
-      e.stopPropagation();
-      const delta = Math.sign(e.deltaY);
-      const SCROLL_SPEED = 50;
-      if (delta > 0 && e.target.id === 'carousel-container') {
-        setCurrentSlide((currentSlide + 1) % totalSlides);
-      } else if (delta < 0 && e.target.id === 'carousel-container') {
-        setCurrentSlide((currentSlide - 1 + totalSlides) % totalSlides);
-      }
-    },
-    [currentSlide, totalSlides]
-  );
+  const goToNextSlide = () => {
+    let index = activeIndex;
+    index = index === length - 1 ? 0 : index + 1;
+    setActiveIndex(index);
+  };
 
+  const renderIndicators = () => {
+    return (
+      <ul className="indicators">
+        {items.map((item, index) => (
+          <li
+            key={item.id}
+            onClick={() => setActiveIndex(index)}
+            className={index === activeIndex ? 'active' : ''}
+          ></li>
+        ))}
+      </ul>
+    );
+  };
+
+  const renderSlides = () => {
+    return items.map((item, index) => (
+      <div
+        key={item.id}
+        className={index === activeIndex ? 'slide active' : 'slide'}
+      >
+        <div className="content">
+          <h2>{item.title}</h2>
+          <p>{item.description}</p>
+        </div>
+        <img
+          src={item.image}
+          alt={item.title}
+          style={{
+            maxWidth: '100%',
+            height: 'auto',
+            display: 'block',
+            margin: '0 auto'
+          }}
+        />
+      </div>
+    ));
+  };
+  
 
   return (
-    <div className="carousel-container" id="carousel-container" onWheel={handleScroll}>
-      <div className="carousel-slide">
-        <a href={images[currentSlide].link} target="_blank" rel="noopener noreferrer">
-          <img
-            src={images[currentSlide].src}
-            alt={images[currentSlide].alt}
-          />
-        </a>
-      </div>
+    <div className="carousel-container">
+      <div className="carousel-inner">{renderSlides()}</div>
+      <button className="carousel-arrow carousel-arrow--left" onClick={goToPrevSlide}>
+        &#10094;
+      </button>
+      <button className="carousel-arrow carousel-arrow--right" onClick={goToNextSlide}>
+        &#10095;
+      </button>
+      <div className="carousel-indicator">{renderIndicators()}</div>
     </div>
   );
 }
